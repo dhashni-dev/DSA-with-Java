a5ec8bd9606069beae6bdd4de0a91f856d851c75
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-06-26 17:31:00 +0530

    Add Kids With Greatest Number of Candies solution
    Implemented a solution to determine which kids can have the greatest number of candies after receiving extra candies.

---
+++ Arrays/KidsWithGreatestNoOfCandies.java
@@ -0,0 +1,34 @@
+/*
+Problem: Kids With the Greatest Number of Candies
+
+Approach:
+Found the maximum number of candies.
+Checked if each kid could reach or exceed the maximum after receiving extra candies.
+*/
+
+import java.util.*;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        int max = candies[0];
+
+        for(int i = 1; i < candies.length; i++) {
+            if(candies[i] > max) {
+                max = candies[i];
+            }
+        }
+
+        List<Boolean> result = new ArrayList<>();
+
+        for(int i = 0; i < candies.length; i++) {
+            if(candies[i] + extraCandies >= max) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+}
