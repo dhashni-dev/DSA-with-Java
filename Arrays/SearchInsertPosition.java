4ed68e828859f79fe38b9c6803669759fc9b9702
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-06-30 22:29:19 +0530

    Document Search Insert Position problem and approach
    Added a problem description and approach for the searchInsert function.

--- Arrays/SearchInsertPosition.java
+++ Arrays/SearchInsertPosition.java
@@ -1,3 +1,11 @@
+/*
+Problem: Search Insert Position
+
+Approach:
+Traversed the sorted array from left to right.
+Returned the first index where nums[i] was greater than or equal to the target.
+If no such index was found, returned nums.length.
+*/
 class Solution {
     public int searchInsert(int[] nums, int target) {
         for(int i = 0;i<nums.length;i++){
