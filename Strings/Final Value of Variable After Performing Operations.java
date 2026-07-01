54602a6d85432cb0b5e6d5acb61fec1246a9ea9f
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-07-01 21:30:22 +0530

    Add comments for problem description and approach
    Added problem statement and approach as comments.

--- Strings/Final Value of Variable After Performing Operations.java
+++ Strings/Final Value of Variable After Performing Operations.java
@@ -1,3 +1,11 @@
+/*
+Problem: Final Value of Variable After Performing Operations
+
+Approach:
+Started with output = 0.
+Traversed all operations.
+Incremented output for "++" operations and decremented output for "--" operations.
+*/
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int output = 0;
