13c0327f9221c08fd50288635282d6fd2942c0eb
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-07-01 21:29:35 +0530

    Implement finalValueAfterOperations method

---
+++ Strings/Final Value of Variable After Performing Operations.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int output = 0;
+
+        for(int i = 0; i < operations.length; i++) {
+            if(operations[i].contains("++")) {
+                output++;
+            } else {
+                output--;
+            }
+        }
+
+        return output;
+    }
+}
