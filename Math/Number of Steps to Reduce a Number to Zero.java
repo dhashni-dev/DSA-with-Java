88cf1c42ce01b835b799070147dc0dce01adba5d
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-07-02 21:43:20 +0530

    Add comments for problem and approach
    Added problem statement and approach as comments.

--- Math/Number of Steps to Reduce a Number to Zero.java
+++ Math/Number of Steps to Reduce a Number to Zero.java
@@ -1,3 +1,12 @@
+/*
+Problem: Number of Steps to Reduce a Number to Zero
+
+Approach:
+Repeatedly checked whether the number was even or odd.
+If even, divided it by 2.
+If odd, subtracted 1.
+Counted each operation until the number became 0.
+*/
 class Solution {
     public int numberOfSteps(int num) {
         int count = 0;
