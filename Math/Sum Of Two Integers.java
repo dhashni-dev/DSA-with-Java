eb95b03505f310d5aaf1e831459cf4bc855e7e73
Dhashni <dhashnisanthamoorthi@gmail.com>
2026-07-02 21:30:37 +0530

    Add problem statement and approach for sum of integers
    Added problem statement and approach for the sum of two integers using bitwise operations.

--- Math/Sum Of Two Integers.java
+++ Math/Sum Of Two Integers.java
@@ -1,3 +1,10 @@
+/*Problem : Sum of Two Integers
+
+Approach:
+
+1. Use the XOR (^) operator to add two numbers without considering carry bits.
+2. Use the AND (&) operator to find carry bits, shift them left by one position (<< 1), and repeat the process until no carry remains.
+*/
 class Solution {
     public int getSum(int a, int b) {
        while (b != 0) {
